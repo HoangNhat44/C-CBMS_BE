207df08fef6156c57545a709e8f0cc214f5c1b4e
NgocHuy89 <huynnhe173359@fpt.edu.vn>
2026-07-13 22:33:03 +0700

    databooking

--- src/modules/product/product.route.js
+++ src/modules/product/product.route.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const productController = require("./product.controller");
-const { authMiddleware, requirePermission } = require("../../middlewares/auth.middleware");
+const { authMiddleware, optionalAuthMiddleware, requirePermission } = require("../../middlewares/auth.middleware");
 
 const router = express.Router();
 
-router.get("/", authMiddleware, requirePermission("VIEW_PRODUCT"), productController.getAllProducts);
-router.get("/:id", authMiddleware, requirePermission("VIEW_PRODUCT"), productController.getProductById);
+router.get("/", optionalAuthMiddleware, productController.getAllProducts);
+router.get("/:id", optionalAuthMiddleware, productController.getProductById);
 router.post("/", authMiddleware, requirePermission("CREATE_PRODUCT"), productController.createProduct);
 router.put("/:id", authMiddleware, requirePermission("UPDATE_PRODUCT"), productController.updateProduct);
 router.delete("/:id", authMiddleware, requirePermission("DELETE_PRODUCT"), productController.deleteProduct);
