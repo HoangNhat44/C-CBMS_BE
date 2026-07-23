3e2ab01b1b90088c8b0d9ffd4cc8cbe1cbbd0959
Hoàng Huy Nhật <nhathhhe173353@fpt.edu.vn>
2026-07-23 21:06:07 +0700

    fix up ảnh
    mở rộng dung lượng up ảnh lên 50mb

--- server.js
+++ server.js
@@ -37,7 +37,8 @@ app.use(cors({
   origin: process.env.FRONTEND_URL || "http://localhost:3000",
   credentials: true,
 }));
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.get("/", (req, res) => {
